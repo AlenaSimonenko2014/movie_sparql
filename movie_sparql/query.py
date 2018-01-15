675df1daca5e691a083b8ecb89a7df188c71b6f0
Alena Simonenko <alenkasim1901@gmail.com>
2018-01-16 03:32:23 +0500

    update query (print)

--- movie_sparql/query.py
+++ movie_sparql/query.py
@@ -16,7 +16,7 @@ SELECT ?musicContributor ?contributor_genre ?name_of_contributor ?name ?genre
  { SERVICE <http://data.linkedmdb.org/sparql>
      { SELECT DISTINCT ?musicContributor 
         WHERE {
-                 ?movie rdfs:label %s ;
+                 ?movie rdfs:label "%s" ;
                         imdb:music_contributor ?musicContributorURI .
                  ?musicContributorURI imdb:music_contributor_name ?musicContributor .
 
@@ -44,28 +44,39 @@ SELECT ?musicContributor ?contributor_genre ?name_of_contributor ?name ?genre
     }
   }
  FILTER regex(?name_of_contributor, CONCAT("^", ?musicContributor, ".*"), "i")
- FILTER (?genre=?contributor_genre)
+ #FILTER (?genre=?contributor_genre)
 
 }
+
+LIMIT 100
 
 """
 
 
-def get_movie(name: str):
+def get_contributors_for_movie(name: str):
     sparql = SPARQLWrapper(GET_MOVIE_SOURCE)
     sparql.setQuery(GET_MOVIE_QUERY % name)
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
+    return_list = []
 
     for result in results["results"]["bindings"]:
+        # ?musicContributor ?contributor_genre ?name_of_contributor ?name ?genre ?who
+        return_list.append({
+            'name': result["musicContributor"]["value"],
+            'genre': result["genre"]["value"],
+            'same_genre_contributor': result["name"]["value"]
+        })
         print(result["name"]["value"])#, '\t\t', result["contributor_genre"]["value"],
              # result["name_of_contributor"]["value"], '\t\t', result["name"]["value"], '\t\t', result["genre"]["value"])
 
-    return {
-        "name": "mimi",
-        "contributors": []
-    }
+    # return_list.append({
+    #     'name': "The Beatles",
+    #     'genre': "Pop Rock",
+    #     'same_genre_contributor': "Killers"
+    # })
+    return return_list
 
 
 if __name__ == '__main__':
-    get_movie('"Yellow Submarine"')
+    get_contributors_for_movie('Meeting People Is Easy')
--- web/views.py
+++ web/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import FormView, TemplateView
 
-from movie_sparql.query import get_movie
+from movie_sparql.query import get_contributors_for_movie
 from web.forms import SearchForm
 from web.models import Movie, Contributor, Genre
 
@@ -16,11 +16,30 @@ class SearchView(FormView):
         """
         If the form is valid, query db and persist data
         """
-        movie_obj = get_movie('"Yellow Submarine"')  # form.data['movie_name'])
+        movie, created = Movie.objects.get_or_create(name=form.data['movie_name'])
 
-        # TODO get_or_create contibutors, genres
+        new_genre_count = new_contributor_count = 0
 
-        movie, created = Movie.objects.get_or_create(name=movie_obj["name"])
+        for obj in get_contributors_for_movie(movie.name):
+            genre, created = Genre.objects.get_or_create(name=obj["genre"])
+            if created:
+                new_genre_count += 1
+
+            contributor, created = Contributor.objects.get_or_create(
+                name=obj["name"], genre=genre)
+            if created:
+                new_contributor_count += 1
+            if contributor not in movie.contributors.all():
+                movie.contributors.add(contributor)
+
+            same_genre_contributor, created = Contributor.objects.get_or_create(
+                name=obj["same_genre_contributor"], genre=genre)
+            if created:
+                new_contributor_count += 1
+
+        print("Added %d new genres" % new_genre_count)
+        print("Added %d new contributors" % new_contributor_count)
+
         return HttpResponseRedirect(reverse('movie-detail', kwargs={'name': movie.name}))
 
 
